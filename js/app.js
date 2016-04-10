ac5e1dec28ace937de307d2edaa38505aa9c73af
Duc Nguyen <ducnn01@gmail.com>
2016-04-10 09:04:56 +0700

    initial

---
+++ js/app.js
@@ -0,0 +1,4 @@
+/**
+ * Created by nnduc on 4/10/16.
+ */
+var angularWithFireBase = angular.module('angularWithFireBase',[]);
---
+++ js/factory.js
@@ -0,0 +1,6 @@
+/**
+ * Created by nnduc on 4/10/16.
+ */
+angularWithFireBase.factory('myFactory',['$scope', function ($scope) {
+
+}]);
